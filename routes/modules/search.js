1518a19d26c3a2b0b7d47072a0f8ad5ea8a0e0e1
jamiennn <jasper07165@hotmail.com>
2023-05-05 17:56:00 +0800

    feat: add date search function

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -4,7 +4,7 @@ const Record = require('../../models/record')
 const Category = require('../../models/category')
 const dateFormat = require('dateformat')
 
-
+//Search by category
 router.get('/:id', (req, res) => {
   const userId = req.user._id
   const categoryId = req.params.id
@@ -21,6 +21,34 @@ router.get('/:id', (req, res) => {
           expense.date = dateFormat(expense.date, "yyyy/mm/dd")
         }))
       res.render('index', { expenses, totalAmount, categorys })
+    })
+})
+
+//Search by date
+router.post('/', (req, res) => {
+  const userId = req.user._id
+  const { searchDate } = req.body
+
+  Record.find({ userId })
+    .lean()
+    .then(expenses => {
+      //過濾出日期一樣的資料
+      return expenses.filter(expense => {
+        const expenseDate = dateFormat(expense.date, 'yyyy-mm')
+        return (expenseDate === searchDate)
+      })
+    })
+    .then(async (expenses) => {
+      let totalAmount = 0
+      const categorys = await Category.find().lean().sort({ id: 'asc' })
+      await Promise.all(
+        expenses.map(async (expense) => {
+          const category = await Category.findById(expense.categoryId).lean()
+          expense.icon = category.icon
+          totalAmount += expense.amount
+          expense.date = dateFormat(expense.date, "yyyy/mm/dd")
+        }))
+      res.render('index', { expenses, totalAmount, categorys, searchDate })
     })
 })
 

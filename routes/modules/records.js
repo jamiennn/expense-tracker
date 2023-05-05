60eb672081846094fe186eda19ed035d4b58787e
jamiennn <jasper07165@hotmail.com>
2023-05-05 15:33:08 +0800

    feat: add form validication to new/edit page

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -4,7 +4,14 @@ const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 const dateFormat = require('dateformat')
+const { body } = require('express-validator')
 
+
+//Setting validator
+const nameLength = (sth) => body(sth).trim().isLength({ min: 1, max: 22 })
+const isDate = (sth) => body(sth).isISO8601().toDate()
+const { isValidObjectId } = require('mongoose')
+const isCost = (sth) => body(sth).trim().isLength({ min: 1, max: 10 })
 
 //Create
 router.get('/new', (req, res) => {
@@ -12,17 +19,20 @@ router.get('/new', (req, res) => {
     .then(categorys => res.render('new', { categorys }))
 })
 
-router.post('/', (req, res) => {
-  const userId = req.user._id
-  const body = req.body
-  if (!body.categoryId) {
-    body.message = '請選擇類別'
-    return res.render('new', { body })
-  }
-  Record.create({ ...body, userId })
-    .then(() => res.redirect('/'))
-    .catch(err => console.log(err))
-})
+router.post('/',
+  nameLength('name'),
+  isDate('date'),
+  isCost('cost'),
+  (req, res) => {
+    const userId = req.user._id
+    const body = req.body
+    if (!isValidObjectId(body.categoryId)) {
+      return res.render('new', { body })
+    }
+    Record.create({ ...body, userId })
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
 
 //Edit
 router.get('/:id/edit', (req, res) => {
@@ -43,21 +53,29 @@ router.get('/:id/edit', (req, res) => {
 })
 
 
-router.put('/:id', (req, res) => {
-  const userId = req.user._id
-  const _id = req.params.id
-  const { name, date, categoryId, amount } = req.body
-  Record.findOne({ _id, userId })
-    .then(expense => {
-      expense.name = name
-      expense.date = date
-      expense.categoryId = categoryId
-      expense.amount = amount
-      return expense.save()
-    })
-    .then(() => res.redirect('/'))
-    .catch(err => console.log(err))
-})
+router.put('/:id',
+  nameLength('name'),
+  isDate('date'),
+  isCost('cost'),
+  (req, res) => {
+    const userId = req.user._id
+    const _id = req.params.id
+    const { name, date, categoryId, amount } = req.body
+
+    if (!isValidObjectId(categoryId)) {
+      return res.render('edit', { ...req.body })
+    }
+    Record.findOne({ _id, userId })
+      .then(expense => {
+        expense.name = name
+        expense.date = date
+        expense.categoryId = categoryId
+        expense.amount = amount
+        return expense.save()
+      })
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
 
 //Delete
 router.delete('/:id', (req, res) => {
